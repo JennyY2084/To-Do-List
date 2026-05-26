946d1762925ab59e97cbcc0da8344cf0a3522977
Jenny Yuan <jenny.yuan@my.bdsc.school.nz>
2026-05-27 09:44:38 +1200

    Imported easygui

--- ToDoListVersion1.py
+++ ToDoListVersion1.py
@@ -1,35 +1,30 @@
+import easygui
 from os import remove
 
 # The function to load the menu and ask the user to input an option.
 def load_menu():
     global list_name
-    print("To Do List Menu")
-    option_1 = "1. Create a new list"
-    option_2 = "2. Load an existing list"
-    option_3 = "3. Delete an existing list"
-    option_4 = "4. Exit"
-    print(option_1)
-    print(option_2)
-    print(option_3)
-    print(option_4)
-    # Ask the user to select an option.
-    user_option = input("Please select an option: ")
+    menu_option = ["1. Create a new list", 
+                   "2. Load an existing list", 
+                   "3. Delete an existing list", "4. Exit"]
+    title = "To Do List Menu"
+    user_option = easygui.choicebox(title, "Please select an option: ", menu_option)
     # Call the corresponding function based on the user's choice, 
     # and pass the tasks list to the function.
-    if user_option == "1":
+    if user_option == "1. Create a new list":
         list_name = ask_list_name_to_create()
         create_list_file()
-    elif user_option == "2":
+    elif user_option == "2. Load an existing list":
         list_name = ask_list_name_to_load()
         load_list_file()
-    elif user_option == "3":
+    elif user_option == "3. Delete an existing list":
         delete_list_file()
-    elif user_option == "4":
-        print("Exiting...")
+    elif user_option == "4. Exit":
+        easygui.msgbox("Exiting...")
     # Prints out an message to tell te user that they 
     # entered an invalid option and reload the menu.
     else:
-        print("Invalid option, please select either 1, 2, or3.")
+        easygui.msgbox("Invalid option, please select either 1, 2, or3.")
         load_menu()
 
 
@@ -39,17 +34,18 @@ task_list = []
 
 # Ask the user to enter the name of the list they want to create or load, and return the name.
 def ask_list_name_to_create():
-    list_name_to_create = input("Please enter the name of the list you want to create: ")
+    list_name_to_create = easygui.enterbox("Please enter the name of the list you want to create: ")
     return list_name_to_create
 def ask_list_name_to_load():
-    list_name_to_load = input("Please enter the name of the list you want to view: ")
+    list_name_to_load = easygui.enterbox("Please enter the name of the list you want to view: ")
     return list_name_to_load
 
 
 # The function to ask the user if they want to add a task to the list, 
 # and call the add task function if they choose yes.
 def add_task_option():
-    add_task_option = input("Do you want to add a task to this list? (yes/no): ")
+    choice = ["yes", "no"]
+    add_task_option = easygui.choicebox("Do you want to add a task to this list?", "Add Task", choice)
     if add_task_option.lower() == "yes":
         add_task(task_list)
     elif add_task_option.lower() == "no":
@@ -104,7 +100,7 @@ def create_list_file():
     global list_name
     try:
         with open(list_name, "r"):
-            print(f"List '{list_name}' already exists, please enter a different name: ")
+            easygui.enterbox(f"List '{list_name}' already exists, please enter a different name: ")
             list_name = ask_list_name_to_create()
             create_list_file()
             return
